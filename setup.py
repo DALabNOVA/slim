51699e2739f6bebdc334579401cc7b73d7281ef2
Rizzolioli <124307964+Rizzolioli@users.noreply.github.com>
2024-10-01 00:37:52 +0100

    updated setup and dist folder for package update

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='gsgp_slim',
-    version='0.1.1',
+    version='0.1.5',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'numpy',
         'pandas',
