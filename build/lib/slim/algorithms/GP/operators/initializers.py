90794d4c9840970d4e98dc8b4258bee0fef84084
DiogoRasteiro <34631781+DiogoRasteiro@users.noreply.github.com>
2024-10-04 14:37:29 +0100

    GP docstring changes

--- build/lib/slim/algorithms/GP/operators/initializers.py
+++ build/lib/slim/algorithms/GP/operators/initializers.py
@@ -46,16 +46,12 @@ def full(size, depth, FUNCTIONS, TERMINALS, CONSTANTS, p_c=0.3):
         The total number of individuals to be generated for the population.
     depth : int
         The maximum depth of the trees.
-    FUNCTIONS : list
-        The list of functions allowed in the trees.
-    TERMINALS : list
-        The list of terminal symbols allowed in the trees.
-    CONSTANTS : list
-        The list of constant values allowed in the trees.
-    input_set_size : int
-        The size of the input set for each individual.
-    umbalanced_obs_ind : list
-        The list of unbalanced observation indices used in creating the random input set.
+    FUNCTIONS : dict
+        The functions allowed in the trees.
+    TERMINALS : dict
+        The symbols allowed in the trees.
+    CONSTANTS : dict
+        The constant values allowed in the trees.
     p_c : float, optional
         The probability of choosing a constant node during tree creation. Default is 0.3.
     Returns
@@ -70,7 +66,7 @@ def full(size, depth, FUNCTIONS, TERMINALS, CONSTANTS, p_c=0.3):
     ]
 
 
-def rhh(init_pop_size, init_depth, FUNCTIONS, TERMINALS, CONSTANTS, p_c=0.3):
+def rhh(size, depth, FUNCTIONS, TERMINALS, CONSTANTS, p_c=0.3):
     """
     Generates a list of individuals with random trees for a GM4OS population using the ramped-half-half method.
 
@@ -80,16 +76,12 @@ def rhh(init_pop_size, init_depth, FUNCTIONS, TERMINALS, CONSTANTS, p_c=0.3):
         The total number of individuals to be generated for the population.
     depth : int
         The maximum depth of the trees.
-    FUNCTIONS : list
+    FUNCTIONS : dict
         The list of functions allowed in the trees.
-    TERMINALS : list
+    TERMINALS : dict
         The list of terminal symbols allowed in the trees.
-    CONSTANTS : list
+    CONSTANTS : dict
         The list of constant values allowed in the trees.
-    input_set_size : int
-        The size of the input set for each individual.
-    umbalanced_obs_ind : list
-        The list of unbalanced observation indices used in creating the random input set.
     p_c : float, optional
         The probability of choosing a constant node during tree creation. Default is 0.3.
     Returns
@@ -100,8 +92,8 @@ def rhh(init_pop_size, init_depth, FUNCTIONS, TERMINALS, CONSTANTS, p_c=0.3):
 
     population = []
 
-    inds_per_bin = init_pop_size / (init_depth - 1)
-    for curr_depth in range(2, init_depth + 1):
+    inds_per_bin = size / (depth - 1)
+    for curr_depth in range(2, depth + 1):
 
         population.extend(
             [
@@ -121,9 +113,9 @@ def rhh(init_pop_size, init_depth, FUNCTIONS, TERMINALS, CONSTANTS, p_c=0.3):
             ]
         )
 
-    while len(population) < init_pop_size:
+    while len(population) < size:
         population.append(
-            create_grow_random_tree(init_depth, FUNCTIONS, TERMINALS, CONSTANTS, p_c)
+            create_grow_random_tree(depth, FUNCTIONS, TERMINALS, CONSTANTS, p_c)
         )
 
     return population
