ddcdb63228567dbe8ab5f4d5f005ee218ddb2a8a
Rizzolioli <124307964+Rizzolioli@users.noreply.github.com>
2024-10-10 23:01:55 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='slim_gsgp',
-    version='0.1.8',
+    version='0.1.10',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -30,5 +30,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.10',
 )
