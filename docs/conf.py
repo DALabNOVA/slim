353fadec47bd49f5d6c863d85a3f30f47cdfebb5
Diogo Rasteiro <34631781+DiogoRasteiro@users.noreply.github.com>
2024-10-12 22:46:56 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,6 +8,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
+autodoc_mock_imports = ['scipy', 'scikit-learn']
 
 project = 'slim_gsgp'
 copyright = '2024, L. Rosenfeld, et al.,'
